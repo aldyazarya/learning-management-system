ddf21ff867e1e2f922c984972f2507eee0b8b6b5
aldy azarya <azarya.dev@gmail.com>
2023-11-06 12:37:55 +0700

    setup uploadthing and create image form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -8,22 +8,21 @@ import { Pencil } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
-import { cn } from "@/lib/utils";
+import { Course } from "@prisma/client";
 
 import {
   Form,
   FormControl,
-  FormMessage,
   FormField,
   FormItem,
+  FormMessage,
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
 
 interface DescriptionFormProps {
-  initialData: {
-    description: string;
-  };
+  initialData: Course;
   courseId: string;
 }
 
@@ -45,7 +44,9 @@ export const DescriptionForm = ({
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData,
+    defaultValues: {
+      description: initialData?.description || "",
+    },
   });
 
   const { isSubmitting, isValid } = form.formState;
@@ -57,20 +58,20 @@ export const DescriptionForm = ({
       toggleEdit();
       router.refresh();
     } catch {
-      toast.error("Something went wrong!");
+      toast.error("Something went wrong");
     }
   };
 
   return (
-    <div className=" mt-6 border bg-slate-100 rounded-md p-4">
-      <div className=" font-medium flex items-center justify-between">
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
         Course description
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing ? (
             <>Cancel</>
           ) : (
             <>
-              <Pencil className=" h-4 w-4 mr-2" />
+              <Pencil className="h-4 w-4 mr-2" />
               Edit description
             </>
           )}
@@ -90,7 +91,7 @@ export const DescriptionForm = ({
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className=" space-y-4 mt-4"
+            className="space-y-4 mt-4"
           >
             <FormField
               control={form.control}
@@ -100,7 +101,7 @@ export const DescriptionForm = ({
                   <FormControl>
                     <Textarea
                       disabled={isSubmitting}
-                      placeholder="e.g. 'This course about...'"
+                      placeholder="e.g. 'This course is about...'"
                       {...field}
                     />
                   </FormControl>
@@ -108,7 +109,7 @@ export const DescriptionForm = ({
                 </FormItem>
               )}
             />
-            <div className=" flex items-center gap-x-2">
+            <div className="flex items-center gap-x-2">
               <Button disabled={!isValid || isSubmitting} type="submit">
                 Save
               </Button>
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -7,6 +7,7 @@ import { IconBadge } from "@/components/icon-badge";
 
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
+import { ImageForm } from "./_components/image-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -50,17 +51,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
       <div className=" grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
         <div>
           <div className=" flex items-center gap-x-2">
-            <IconBadge  icon={LayoutDashboard} />
+            <IconBadge icon={LayoutDashboard} />
             <h2 className=" text-xl">Customize your course</h2>
           </div>
-          <TitleForm
-            initialData={course}
-            courseId={course.id}
-          />
-          <DescriptionForm
-            initialData={course}
-            courseId={course.id}
-          />
+          <TitleForm initialData={course} courseId={course.id} />
+          <DescriptionForm initialData={course} courseId={course.id} />
+          <ImageForm initialData={course} courseId={course.id} />
         </div>
       </div>
     </div>
