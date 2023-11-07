4fd5aa87f2bff5110028be28d97c241f8410ee0c
aldy azarya <azarya.dev@gmail.com>
2023-11-07 19:13:10 +0700

    Chapter Description form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -5,7 +5,9 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import { IconBadge } from "@/components/icon-badge";
+
 import { ChapterTitleForm } from "./_components/chapter-title-form";
+import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 
 const ChapterIdPage = async ({
   params,
@@ -70,6 +72,11 @@ const ChapterIdPage = async ({
             initialData={chapter}
             courseId={params.courseId}
             chapterId={params.chapterId}
+          />
+          <ChapterDescriptionForm 
+                initialData={chapter}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
           />
         </div>
       </div>
